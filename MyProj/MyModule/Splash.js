d5b056181ea90df3fc313ef4fbf4ee91eeb434a0
rock <wang1ran@163.com>
2017-05-20 14:26:25 +0800

    Revert "nothing"
    This reverts commit 007af26665fdba037f24cc360f56f7aea6a26774.

--- MyProj/MyModule/Splash.js
+++ MyProj/MyModule/Splash.js
@@ -3,12 +3,10 @@ import {Image,View,Text} from 'react-native'
 import NetworkManager from './network/NetworkManager'
 import { StackNavigator } from 'react-navigation';
 
-import { NavigationActions } from 'react-navigation';
-
 export default class Splash extends Component{
 
-	constructor(props){
-		super(props);
+	constructor(){
+		super();
 		this.state = {
 			results : ''
 		};
@@ -17,30 +15,15 @@ export default class Splash extends Component{
 		//http的链接会加载不出图片,https://segmentfault.com/a/1190000002933776 通过xcode来修改配置
 		networkManager.getBenifits(1,1,(responseJson)=>{
 			this.setState({results : responseJson.results[0].url});
-			const { navigate } = this.props.navigator;
-			setInterval(()=>{
-				navigate('MainComponent');
-			},5000);
-
-		// setInterval(()=>{
-		// 	const navigateAction = NavigationActions.navigate({
-		//      routeName: 'MainComponent',
-		//      params: {},
-
-	 //  			// navigate can have a nested navigate action that will be run inside the child router
-  // 			 action: NavigationActions.navigate({ routeName: 'MainComponent'})
-		// 	})
-
-		// 	let p = this.props;
-		// 	let nav = p.navigation;
-		// 	nav.dispatch(navigateAction)
-		// },5000);
 		});
 		
 	}
 
 	componentDidMount(){
-		
+		const { navigate } = this.props.navigation;
+		setInterval(()=>{
+			navigate('MainComponent');
+		},5000);
 		
 	}
 
